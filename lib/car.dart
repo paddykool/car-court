85f58ce97ce45858740d550d3db20a83d954aad2
paddykool <pjmullan@gmail.com>
2022-07-10 12:05:58 +0800

    adding dummy car data and car card

--- lib/car.dart
+++ lib/car.dart
@@ -3,6 +3,7 @@
 class Car {
   final MediaType? mediaType;
   final String? name;
+  final String? url;
   final String? description;
   final int? year;
   final FuelType? fuelType;
@@ -10,10 +11,41 @@ class Car {
   const Car({
     this.mediaType,
     this.name,
+    this.url,
     this.description,
     this.year,
     this.fuelType,
   });
+
+  static List<Car> cars = [
+    const Car(
+      mediaType: MediaType.image,
+      name: "Big BMW",
+      description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas commodo quam libero, vel laoreet ipsum consectetur id. In tristique magna at orci imperdiet luctus. Ut pretium suscipit mollis. Aliquam sagittis pharetra dolor, in venenatis quam viverra sit amet. Integer placerat placerat orci a tincidunt. Sed porta quis justo eget egestas.",
+      fuelType: FuelType.diesel,
+      year: 1998,
+      url: 'assets/BMW_750iL.jpeg',
+    ),
+    const Car(
+      mediaType: MediaType.image,
+      name: "Big Merc",
+      description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas commodo quam libero, vel laoreet ipsum consectetur id. In tristique magna at orci imperdiet luctus. Ut pretium suscipit mollis. Aliquam sagittis pharetra dolor, in venenatis quam viverra sit amet. Integer placerat placerat orci a tincidunt. Sed porta quis justo eget egestas.",
+      fuelType: FuelType.petrol,
+      year: 2002,
+      url: 'assets/Merc_B_Class.jpeg',
+    ),
+    const Car(
+      mediaType: MediaType.image,
+      name: "Big Ford",
+      description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas commodo quam libero, vel laoreet ipsum consectetur id. In tristique magna at orci imperdiet luctus. Ut pretium suscipit mollis. Aliquam sagittis pharetra dolor, in venenatis quam viverra sit amet. Integer placerat placerat orci a tincidunt. Sed porta quis justo eget egestas.",
+      fuelType: FuelType.diesel,
+      year: 2011,
+      url: 'assets/Focus.jpeg',
+    ),
+  ];
 }
 
 /// Used to specify whether the car picture is an image or video
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:car_court/car.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key, required this.title}) : super(key: key);
@@ -17,7 +18,47 @@ class _HomePageState extends State<HomePage> {
         title: Text(widget.title),
       ),
       body: SafeArea(
-        child: Container(),
+        child: ListView.builder(
+          itemCount: Car.cars.length,
+          itemBuilder: (context, index) => _buildCarCard(Car.cars[index]),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCarCard(Car car) {
+    return Card(
+      elevation: 4,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16.0)),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image(
+              image: AssetImage(car.url!),
+            ),
+          ),
+          Text(
+            car.name!,
+            style: const TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+            car.year!.toString(),
+            style: const TextStyle(
+              fontSize: 14,
+            ),
+          ),
+          Text(
+            car.description!,
+            style: const TextStyle(
+              fontSize: 14,
+            ),
+          ),
+          const SizedBox(height: 8),
+        ],
       ),
     );
   }
