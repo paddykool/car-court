a9254073335d605fc9a5b64da6b00b81b50da482
paddykool <pjmullan@gmail.com>
2022-07-13 15:52:53 +0800

    adding stack to cara details page

--- lib/car.dart
+++ lib/car.dart
@@ -22,7 +22,7 @@ class Car {
       mediaType: MediaType.image,
       name: "Big BMW",
       description:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas commodo quam libero, vel laoreet ipsum consectetur id. In tristique magna at orci imperdiet luctus. Ut pretium suscipit mollis. Aliquam sagittis pharetra dolor, in venenatis quam viverra sit amet. Integer placerat placerat orci a tincidunt. Sed porta quis justo eget egestas.",
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas commodo quam libero, vel laoreet ipsum consectetur id. In tristique magna at orci imperdiet luctus. Ut pretium suscipit mollis. Aliquam sagittis pharetra dolor, in venenatis quam viverra sit amet. Integer placerat placerat orci a tincidunt. Sed porta quis justo eget egestas. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas commodo quam libero, vel laoreet ipsum consectetur id. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas commodo quam libero, vel laoreet ipsum consectetur id. In tristique magna at orci imperdiet luctus. Ut pretium suscipit mollis. Aliquam sagittis pharetra dolor, in venenatis quam viverra sit amet. Integer placerat placerat orci a tincidunt. Sed porta quis justo eget egestas. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas commodo quam libero, vel laoreet ipsum consectetur id.",
       fuelType: FuelType.diesel,
       year: 1998,
       url: 'assets/BMW_750iL.jpeg',
@@ -31,7 +31,7 @@ class Car {
       mediaType: MediaType.image,
       name: "Big Merc",
       description:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas commodo quam libero, vel laoreet ipsum consectetur id. In tristique magna at orci imperdiet luctus. Ut pretium suscipit mollis. Aliquam sagittis pharetra dolor, in venenatis quam viverra sit amet. Integer placerat placerat orci a tincidunt. Sed porta quis justo eget egestas.",
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas commodo quam libero, vel laoreet ipsum consectetur id. In tristique magna at orci imperdiet luctus. Ut pretium suscipit mollis. Aliquam sagittis pharetra dolor, in venenatis quam viverra sit amet. Integer placerat placerat orci a tincidunt. Sed porta quis justo eget egestas. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas commodo quam libero, vel laoreet ipsum consectetur id. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas commodo quam libero, vel laoreet ipsum consectetur id. In tristique magna at orci imperdiet luctus. Ut pretium suscipit mollis. Aliquam sagittis pharetra dolor, in venenatis quam viverra sit amet. Integer placerat placerat orci a tincidunt. Sed porta quis justo eget egestas. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas commodo quam libero, vel laoreet ipsum consectetur id.",
       fuelType: FuelType.petrol,
       year: 2002,
       url: 'assets/Merc_B_Class.jpeg',
@@ -40,7 +40,7 @@ class Car {
       mediaType: MediaType.image,
       name: "Big Ford",
       description:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas commodo quam libero, vel laoreet ipsum consectetur id. In tristique magna at orci imperdiet luctus. Ut pretium suscipit mollis. Aliquam sagittis pharetra dolor, in venenatis quam viverra sit amet. Integer placerat placerat orci a tincidunt. Sed porta quis justo eget egestas.",
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas commodo quam libero, vel laoreet ipsum consectetur id. In tristique magna at orci imperdiet luctus. Ut pretium suscipit mollis. Aliquam sagittis pharetra dolor, in venenatis quam viverra sit amet. Integer placerat placerat orci a tincidunt. Sed porta quis justo eget egestas. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas commodo quam libero, vel laoreet ipsum consectetur id. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas commodo quam libero, vel laoreet ipsum consectetur id. In tristique magna at orci imperdiet luctus. Ut pretium suscipit mollis. Aliquam sagittis pharetra dolor, in venenatis quam viverra sit amet. Integer placerat placerat orci a tincidunt. Sed porta quis justo eget egestas. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas commodo quam libero, vel laoreet ipsum consectetur id.",
       fuelType: FuelType.diesel,
       year: 2011,
       url: 'assets/Focus.jpeg',
--- lib/car_details.dart
+++ lib/car_details.dart
@@ -14,6 +14,9 @@ class CarDetails extends StatefulWidget {
 }
 
 class _CarDetailsState extends State<CarDetails> {
+  double _sliderScalar = 1.0;
+  final TransformationController _controller = TransformationController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,52 +24,135 @@ class _CarDetailsState extends State<CarDetails> {
         title: (Text(widget.car.name!)),
       ),
       body: SafeArea(
-          child: ListView(
-        children: [
-          SizedBox(
-            width: double.infinity,
-            child: Image(
-              image: AssetImage(widget.car.url!),
+        // TODO - try to have just a listView here?
+        child: Stack(
+          children: [
+            // This is the picture of the car...
+            Positioned(
+              height: MediaQuery.of(context).size.height * 0.4,
+              width: MediaQuery.of(context).size.width,
+              // TODO - can this go into a sized box?
+              // TODO - what exactlh is boxfit?
+              child: InteractiveViewer(
+                transformationController: _controller,
+                child: Image(
+                  image: AssetImage(widget.car.url!),
+                  fit: BoxFit.fitWidth,
+                ),
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  widget.car.year!.toString(),
-                  style: const TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
+            // This is everything below the picture
+            // TODO - Why do we even need a stack here? - Try listView
+            Positioned(
+              top: MediaQuery.of(context).size.height * 0.4,
+              height: MediaQuery.of(context).size.height * 0.6 - 50,
+              width: MediaQuery.of(context).size.width,
+              child: ListView(
+                children: [
+                  Slider(
+                    min: 1,
+                    max: 4.0,
+                    divisions: 20,
+                    value: _sliderScalar,
+                    onChanged: (value) {
+                      setState(() {
+                        _sliderScalar = value;
+                        _controller.value =
+                            Matrix4.identity().scaled(_sliderScalar);
+                      });
+                    },
+                    activeColor: Colors.purple[800],
+                    inactiveColor: Colors.purple[200],
                   ),
-                ),
-                const SizedBox(height: 4),
-                Text(
-                  widget.car.name!,
-                  style: const TextStyle(
-                    fontSize: 18,
+                  Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          widget.car.year!.toString(),
+                          style: const TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(height: 4),
+                        Text(
+                          widget.car.name!,
+                          style: const TextStyle(
+                            fontSize: 18,
+                          ),
+                        ),
+                        const SizedBox(height: 4),
+                        const Text(
+                          'Copyright: P to the K',
+                          style: TextStyle(
+                            fontSize: 16,
+                          ),
+                        ),
+                        const SizedBox(height: 8),
+                        Text(
+                          widget.car.description!,
+                          style: const TextStyle(
+                            fontSize: 16,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                const SizedBox(height: 4),
-                const Text(
-                  'Copyright: P to the K',
-                  style: TextStyle(
-                    fontSize: 16,
-                  ),
-                ),
-                const SizedBox(height: 8),
-                Text(
-                  widget.car.description!,
-                  style: const TextStyle(
-                    fontSize: 16,
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
-      )),
+          ],
+        ),
+      ),
     );
   }
 }
+
+// ListView(
+// children: [
+// SizedBox(
+// width: double.infinity,
+// child: Image(
+// image: AssetImage(widget.car.url!),
+// ),
+// ),
+// Padding(
+// padding: const EdgeInsets.all(12.0),
+// child: Column(
+// crossAxisAlignment: CrossAxisAlignment.start,
+// children: [
+// Text(
+// widget.car.year!.toString(),
+// style: const TextStyle(
+// fontSize: 18,
+// fontWeight: FontWeight.bold,
+// ),
+// ),
+// const SizedBox(height: 4),
+// Text(
+// widget.car.name!,
+// style: const TextStyle(
+// fontSize: 18,
+// ),
+// ),
+// const SizedBox(height: 4),
+// const Text(
+// 'Copyright: P to the K',
+// style: TextStyle(
+// fontSize: 16,
+// ),
+// ),
+// const SizedBox(height: 8),
+// Text(
+// widget.car.description!,
+// style: const TextStyle(
+// fontSize: 16,
+// ),
+// ),
+// ],
+// ),
+// ),
+// ],
+// ),
