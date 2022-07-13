60c501669b54fc5330e8823ea7d68ece44c58af8
paddykool <pjmullan@gmail.com>
2022-07-13 16:19:12 +0800

    Swapped Stack for Column with embedded ListView

--- lib/car_details.dart
+++ lib/car_details.dart
@@ -25,45 +25,35 @@ class _CarDetailsState extends State<CarDetails> {
       ),
       body: SafeArea(
         // TODO - try to have just a listView here?
-        child: Stack(
+        child: Column(
           children: [
             // This is the picture of the car...
-            Positioned(
-              height: MediaQuery.of(context).size.height * 0.4,
-              width: MediaQuery.of(context).size.width,
-              // TODO - can this go into a sized box?
-              // TODO - what exactlh is boxfit?
-              child: InteractiveViewer(
-                transformationController: _controller,
-                child: Image(
-                  image: AssetImage(widget.car.url!),
-                  fit: BoxFit.fitWidth,
-                ),
+            InteractiveViewer(
+              transformationController: _controller,
+              child: Image(
+                image: AssetImage(widget.car.url!),
+                fit: BoxFit.fitWidth,
               ),
             ),
             // This is everything below the picture
             // TODO - Why do we even need a stack here? - Try listView
-            Positioned(
-              top: MediaQuery.of(context).size.height * 0.4,
-              height: MediaQuery.of(context).size.height * 0.6 - 50,
-              width: MediaQuery.of(context).size.width,
+            Slider(
+              min: 1,
+              max: 4.0,
+              divisions: 20,
+              value: _sliderScalar,
+              onChanged: (value) {
+                setState(() {
+                  _sliderScalar = value;
+                  _controller.value = Matrix4.identity().scaled(_sliderScalar);
+                });
+              },
+              activeColor: Colors.purple[800],
+              inactiveColor: Colors.purple[200],
+            ),
+            Expanded(
               child: ListView(
                 children: [
-                  Slider(
-                    min: 1,
-                    max: 4.0,
-                    divisions: 20,
-                    value: _sliderScalar,
-                    onChanged: (value) {
-                      setState(() {
-                        _sliderScalar = value;
-                        _controller.value =
-                            Matrix4.identity().scaled(_sliderScalar);
-                      });
-                    },
-                    activeColor: Colors.purple[800],
-                    inactiveColor: Colors.purple[200],
-                  ),
                   Padding(
                     padding: const EdgeInsets.all(12.0),
                     child: Column(
